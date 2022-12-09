26469b5ad89cf35253b12e0540bab4bc2560bf0e
locoop11 <120200289+locoop11@users.noreply.github.com>
2022-12-09 19:02:25 +0000

    New constants for requests

--- src/main.py
+++ src/main.py
@@ -1,16 +1,8 @@
-#  Dicionario com a lista de driver disponiveis indexados por ano, mes, dia e hora
-scheduledDrivers = {2022: {1: {1: { 12: ["Maria", "John"]}}}}
+import readingFromFiles
+import constants as const
 
-# Dicionario com a lista de drivers e as suas caracteristicas
-drivers = {"John": {"stars": 4, "languages": ["English","Spanish"], "tarifa": "80", 
-                    "especializaçao": "conforto", "tempo_max": "10", "horas_acomuladas": "3"} ,
-          "Maria": {"stars": 4, "languages": ["Portuguese","English"], "tarifa": "40", 
-                    "especializaçao":"velocidade", "tempo_max": "30", "horas_acomuladas": "13"}
-          }
-
-def getDriversByDate(year, month, day, hour):
-    return scheduledDrivers.get(year, {}).get(month, {}).get(day, {}).get(hour, [])
-
+skippersDict = {}
+drivers = skippersDict
 
 def getMatchingDrivers(availableDrivers, stars, language, especializaçao, tempo_requesitado):
 #funcao para unir condutor e o cliente
@@ -38,22 +30,10 @@ print(matchingDriver)
 
 def readSkippersFromFile ( skippersFileName ): 
     # Ler o ficheiro
-    f = open(skippersFileName, "r")
-    fileLineContents = f.readlines()
-    skippers = {}
+    skippersList = readingFromFiles.readSkippersFile(skippersFileName)
+    skippersDict = {}
 
     # Iterar sobre cada linha do ficheiro
-    for x in range(7, len(fileLineContents)):
-        skipperRaw = fileLineContents[x].strip()
-
-        # Verificar se a linha nao esta vazia
-        if( skipperRaw != "" ):
-            # Fazer split da linha
-            caracteristicasSkipper = skipperRaw.split(", ")
-            # Adicionar o skipper ao dicionario
-            skippers[caracteristicasSkipper[0]]= {"stars": caracteristicasSkipper[2], "languages": caracteristicasSkipper[1], "tarifa": caracteristicasSkipper[3], "especializaçao": caracteristicasSkipper[4], "tempo_max": caracteristicasSkipper[5], "horas_acomuladas": caracteristicasSkipper[6]}
-    f.close()
-    return skippers
 
 
 mySkipers = readSkippersFromFile ("./data/skippers.txt")
--- src/readingFromFiles.py
+++ src/readingFromFiles.py
@@ -1,4 +1,4 @@
-import constants
+import constants as const
 
 #-*- coding: utf-8 -*-
 
@@ -9,14 +9,14 @@ import constants
 
 def teste():
   
-    print(readRequestsFile("./data/testSet1/requests17h00.txt"))
+    print(readSkippersFile("./data/testSet1/skippers17h00.txt"))
 
 
 
 def removeHeader (fileName):
     file = open(fileName, "r")
     filecontent = file.readlines()
-    for x in range(constants.NUM_HEADER_LINES):
+    for x in range(const.NUM_HEADER_LINES):
         del filecontent[0]
     file.close()
     return filecontent
@@ -44,8 +44,17 @@ def readSkippersFile(fileName):
     for skipper in inFile:
         skipperData = skipper.rstrip().split(", ")
         skippersList.append(skipperData)
+    
+    skippersDict = {}
+    for skipper in skippersList:
+        if( skipper != "" ):
+            # Fazer split da linha
+            # Adicionar o skipper ao dicionario
+            skippersDict[skipper[const.SKIPPER_NAME_IDX]]= {"licenceType": skipper[const.SKIPPER_LICENCE_TYPE], "languages": skipper[const.SKIPPER_LANGUAGES],
+                                   "tariff": skipper[const.SKIPPER_TARIFF], "specialities": skipper[const.SKIPPER_SPECIALTIES], 
+                                   "timeMax": skipper[const.SKIPPER_TIME_MAX], "accumulatedTime": skipper[const.SKIPPER_ACCUMULATED_TIME]}
+    return skippersDict
 
-    return skippersList
 
 def readRequestsFile(fileName):
     """
@@ -60,5 +69,16 @@ def readRequestsFile(fileName):
     for line in inFile:
         requestData = line.rstrip().split(", ")
         requestsList.append(requestData)        
+   
+   
+    requestDict = {}
+    for request in requestsList:
+        if( request != "" ):
+            requestDict[request[const.REQUEST_CLIENT_NAME_IDX]]= {"requestLicenceType": request[const.SKIPPER_LICENCE_TYPE],
+                                 "languages": request[const.REQUEST_CLIENT_LANGUAGES],
+                                "specialities": request[const.REQUEST_SPECIALITY_TYPE], 
+                                   "requestTime": request[const.REQUEST_CRUISE_TIME]}
+    return requestDict
 
-    return requestsList
+
+teste()
