4907fd326379ef0d9042a61dd630db971c7fd813
Hugo Silva <hugo.locop@gmail.com>
2022-12-13 15:04:26 +0000

    stable version of biggestDate

--- src/scheduling.py
+++ src/scheduling.py
@@ -35,5 +35,3 @@ def updateSkippers(skippers, schedule):
 
 
 
-
-
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import sys
-
-
+import constants as const
+import dateTime
 # Read command line arguments from sys.argv and validate file names and return List of file.
 # Executing this program should in the form: python3 update.py inputFile1 inputFile2 inputFile3
 # The return List should be the followng
@@ -16,9 +16,59 @@ def readCommandLineArguments():
 # Update the schedule. Give the matched skipper details, the request he was just assigned to and the existing schedules
 # this function returns the new schedule
 def updateSchedule(skippersRecord, request, schedulesDict):
-    """ TODO: Not implemented yet """
+    """implemented yet """
+    dateOfLastTrip = "1:1:0001|00:00"
+    for schedulesKeys in schedulesDict.keys():
+        if skippersRecord["name"] == schedulesKeys.split("-")[1]: 
+            if BiggestDate(schedulesKeys.split("-")[0], dateOfLastTrip) != dateOfLastTrip:
+                dateOfLastTrip = schedulesKeys.split("-")[0]
 
 
 # Update the skipper. Given the macthed skypper details return a new updated skipper record based on the travel request
 def updateSkipper(skipperRecord, request):
     """ TODO: Implement this"""
+
+
+def BiggestDate(dateTime1, dateTime2):
+    """
+    This funccion is used to give the biggest date between two
+    requires:
+    
+    
+    
+    
+    
+    
+    """
+    date1 = ""+dateTime1.split("|")[0]
+    date2 = ""+dateTime2.split("|")[0]
+    day1 = ""+date1.split(":")[0]
+    month1 = ""+date1.split(":")[1]
+    year1 = ""+date1.split(":")[2]
+    day2 = ""+date2.split(":")[0]
+    month2 = ""+date2.split(":")[1]
+    year2 = ""+date2.split(":")[2]
+    time1 = ""+dateTime1.split("|")[1]
+    time2 = ""+dateTime2.split("|")[1]
+    hour1 = ""+time1.split(":")[0]
+    min1 = ""+time1.split(":")[1]
+    hour2 = ""+time2.split(":")[0]
+    min2 = ""+time2.split(":")[1]
+
+    if date1 == date2 :
+        if int ( ""+hour1 + min1 )  >  int( ""+hour2 + min2 ):
+            return dateTime1
+        else:
+            return dateTime2
+        
+    else:
+        if int(""+year1+month1+day1) > int(""+year2+month2+day2):
+            print(int(year1+month1+day1))
+            print(int(year2+month2+day2))
+            return dateTime1
+        else:
+            return dateTime2
+            
+
+
+print(BiggestDate("00:12:3000|14:00", "01:12:2023|15:00"))
